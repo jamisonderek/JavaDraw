2342f030d7022b066d6cc0d789956bf719617b45
Derek Jamison <derekj@outlook.com>
2017-05-02 21:37:13 -0700

    Show JFrame

---
+++ src/Demo.java
@@ -0,0 +1,19 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Demo implements Runnable {
+	public static void main(String[] args) {
+		Runnable runnable = new Demo();
+        SwingUtilities.invokeLater(runnable);
+	}
+	
+	public void run() {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("My Demo program");
+		frame.setPreferredSize(new Dimension(640,480));
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
