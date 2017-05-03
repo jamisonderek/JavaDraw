a4332074eb3415fb9612cc24392caed634b8d7a3
Derek Jamison <derekj@microsoft.com>
2017-05-02 21:50:11 -0700

    Render text at various locations.

--- src/SamplePanel.java
+++ src/SamplePanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
@@ -9,5 +10,13 @@ public class SamplePanel extends JPanel {
         // Fill background of our panel with blue
         g.setColor(Color.blue);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        
+        // Draw yellow text
+        g.setColor(Color.yellow);
+        Font font = new Font("Arial", Font.BOLD, 38);
+        g.setFont(font);
+        g.drawString("(30,30)", 30, 30);
+        g.drawString("(230,30)", 230, 30);
+        g.drawString("(30,230)", 30, 230);
     }
 }
