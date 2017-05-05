db76bd841137c8e4d4211fccb71d1c1e874e42e8
Derek Jamison <derekj@microsoft.com>
2017-05-05 09:25:27 -0700

    Added getMouseCursor

--- src/SamplePanel2.java
+++ src/SamplePanel2.java
@@ -3,15 +3,18 @@ import java.awt.event.*;
 import java.awt.geom.*;
 import java.util.ArrayList;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 @SuppressWarnings("serial")
 public class SamplePanel2 extends JPanel {
-
+	
 	ArrayList<Shape> shapes;
-	int x = -1;
-	int y = -1;
+	Rectangle2D mouseClickRect = new Rectangle2D.Double();
 
 	SamplePanel2() {
+
+		setBackground(Color.blue);
+		
 		shapes = new ArrayList<Shape>();
 
 		shapes.add(new Line2D.Double(20, 20, 30, 30));
@@ -29,6 +32,7 @@ public class SamplePanel2 extends JPanel {
 		shapes.add(new RoundRectangle2D.Double(50, 280, 150, 50, 30, 30));
 
 		shapes.add(new Ellipse2D.Double(50, 350, 150, 50));
+		shapes.add(new Ellipse2D.Double(170, 350, 150, 50));
 
 		shapes.add(new Arc2D.Double(350, 350, 150, 50, 45, 270, Arc2D.PIE));
 
@@ -44,8 +48,7 @@ public class SamplePanel2 extends JPanel {
 	}
 
 	private void pressed(int x, int y) {
-		this.x = x;
-		this.y = y;
+		mouseClickRect.setRect(x, y, 1, 1);
 		repaint();
 	}
 
@@ -53,19 +56,36 @@ public class SamplePanel2 extends JPanel {
 		super.paintComponent(graphics);
 		Graphics2D g = (Graphics2D) graphics;
 
-		g.setColor(Color.blue);
-		g.fillRect(0, 0, this.getWidth(), this.getHeight());
-
+		Rectangle2D mouseCursorRect = getMouseCursor();
+		
 		for (Shape s : shapes) {
-			if (s.intersects(x, y, 1, 1)) {
+			if (s.intersects(mouseClickRect)) {
+				float x = (float)mouseClickRect.getX();
+				float y = (float)mouseClickRect.getY();
 				g.setPaint(new GradientPaint(x, y, Color.RED, x + 10, y + 30, Color.YELLOW));
 				g.fill(s);
-				g.setPaint(Color.blue);
+			} else if (s.intersects(mouseCursorRect)) {
+					float x = (float)mouseCursorRect.getX();
+					float y = (float)mouseCursorRect.getY();
+					g.setPaint(new GradientPaint(x, y, Color.GREEN, x + 30, y + 10, Color.BLACK));
+					g.fill(s);
 			} else {
-				g.setColor(Color.green);
+				g.setPaint(Color.yellow);
 				g.draw(s);
 			}
 		}
+		
+		// Repaint so that we paint latest mouseCursor.
+		repaint();
+	}
+	
+	private Rectangle2D getMouseCursor()
+	{
+		Point cursor = MouseInfo.getPointerInfo().getLocation();
+		Point locataionOfJPanel = new Point(0,0);
+		SwingUtilities.convertPointToScreen(locataionOfJPanel, this);		
+		Rectangle2D mouseCursor = new Rectangle2D.Double(cursor.getX()-locataionOfJPanel.getX(), cursor.getY()-locataionOfJPanel.getY(), 1, 1); 		
+		return mouseCursor;
 	}
 	
 	private Shape MyShape(int x, int y) {
